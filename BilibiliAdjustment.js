30bfaecf87ca3f6c126a7b74f97b1c2cfc6749ce
仇灾幼 <26304262+QIUZAIYOU@users.noreply.github.com>
2024-03-08 23:44:59 +0800

    Update BilibiliAdjustment.js

--- BilibiliAdjustment.js
+++ BilibiliAdjustment.js
@@ -3,7 +3,7 @@
 // @namespace         哔哩哔哩（bilibili.com）调整 - 纯原生JS版
 // @copyright         QIAN
 // @license           GPL-3.0 License
-// @version           0.1.15
+// @version           0.1.16
 // @description       一、首页新增推荐视频历史记录(仅记录前6个推荐位中的非广告内容)，以防误点刷新错过想看的视频。二、动态页调整：默认显示"投稿视频"内容，可自行设置URL以免未来URL发生变化。三、播放页调整：1.自动定位到播放器（进入播放页，可自动定位到播放器，可设置偏移量及是否在点击主播放器时定位；）；2.可设置播放器默认模式；3.可设置是否自动选择最高画质；4.新增快速返回播放器漂浮按钮；5.新增点击评论区时间锚点可快速返回播放器；6.网页全屏模式解锁(网页全屏模式下可滚动查看评论，并在播放器控制栏新增快速跳转至评论区按钮)；7.将视频简介内容优化后插入评论区或直接替换原简介区内容(替换原简介中固定格式的静态内容为跳转链接)；8.视频播放过程中跳转指定时间节点至目标时间节点(可用来跳过片头片尾及中间广告等)；9.新增点击视频合集、下方推荐视频、结尾推荐视频卡片快速返回播放器；
 // @author            QIAN
 // @match             *://www.bilibili.com
@@ -23,9 +23,6 @@
 // @homepageURL       https://github.com/QIUZAIYOU/Bilibili-VideoPage-Adjustment-Further
 // @icon              https://www.bilibili.com/favicon.ico?v=1
 // ==/UserScript==
-
-// TODO:修复番剧页面插入脚本设置选项不生效的问题
-
 (function () {
   'use strict';
   let vars = {
@@ -75,6 +72,7 @@
     videoNextPlayAndRecommendLink: '.video-page-card-small .card-box',
     videoSectionsEpisodeLink: '.video-sections-content-list .video-episode-card',
     videoMultiPageLink: '#multi_page ul li',
+    bangumiApp: '#__next',
     bangumiComment: '#comment_module',
     bangumiFloatNav: '#__next div[class*="navTools_floatNavExp"] div[class*="navTools_navMenu"]',
     bangumiMainContainer: '.main-container',
@@ -105,7 +103,7 @@
     clearRecordsButton: '#clearRecordsButton',
     saveRecordsButton: '#saveRecordsButton',
     uploadSkipTimeNodesButton: '#uploadSkipTimeNodesButton',
-    indexBodyElement: '#i_cecream',
+    indexApp: '#i_cecream',
     indexRecommendVideoSix: '.recommended-container_floor-aside .feed-card:nth-child(-n+7)',
     indexRecommendVideoRollButtonWrapper: '.recommended-container_floor-aside .feed-roll-btn',
     indexRecommendVideoHistoryPopoverTitle: '#indexRecommendVideoHistoryPopoverTitle',
@@ -1541,13 +1539,13 @@
       utils.createElementAndInsert(indexRecommendVideoHistoryOpenButtonHtml, $indexRecommendVideoRollButtonWrapper, 'append')
       const $indexRecommendVideoHistoryPopover = utils.createElementAndInsert(indexRecommendVideoHistoryPopoverHtml, document.body, 'append')
       $indexRecommendVideoHistoryPopover.addEventListener('toggle', async (event) => {
-        const [ $indexBodyElement, $indexRecommendVideoHistoryPopoverTitle ] = await elmGetter.get([ selectors.indexBodyElement, selectors.indexRecommendVideoHistoryPopoverTitle ])
+        const [ $indexApp, $indexRecommendVideoHistoryPopoverTitle ] = await elmGetter.get([ selectors.indexApp, selectors.indexRecommendVideoHistoryPopoverTitle ])
         if (event.newState === 'open') {
-          $indexBodyElement.style.pointerEvents = 'none'
+          $indexApp.style.pointerEvents = 'none'
           $indexRecommendVideoHistoryPopoverTitle.querySelector('span').append(`(${$indexRecommendVideoHistoryPopover.querySelector('ul').childElementCount})`)
         }
         if (event.newState === 'closed') {
-          $indexBodyElement.style.pointerEvents = 'auto'
+          $indexApp.style.pointerEvents = 'auto'
           $indexRecommendVideoHistoryPopoverTitle.querySelector('span').innerText = '首页视频推荐历史记录'
         }
       })
@@ -1726,7 +1724,8 @@
         GM_registerMenuCommand('设置', () => {
           $videoSettingPopover.showPopover()
         })
-        const [ $app, $IsVip, $AutoLocate, $AutoLocateVideo, $AutoLocateBangumi, $TopOffset, $ClickPlayerAutoLocation, $AutoQuality, $Quality4K, $Quality8K, $Checkbox4K, $Checkbox8K, $WebfullUnlock, $AutoReload, $videoSettingSaveButton ] = await elmGetter.get([ selectors.app, selectors.IsVip, selectors.AutoLocate, selectors.AutoLocateVideo, selectors.AutoLocateBangumi, selectors.TopOffset, selectors.ClickPlayerAutoLocation, selectors.AutoQuality, selectors.Quality4K, selectors.Quality8K, selectors.Checkbox4K, selectors.Checkbox8K, selectors.WebfullUnlock, selectors.AutoReload, selectors.videoSettingSaveButton ])
+        const $app = vals.player_type() === 'video' ? await elmGetter.get(selectors.app) : await elmGetter.get(selectors.bangumiApp)
+        const [ $IsVip, $AutoLocate, $AutoLocateVideo, $AutoLocateBangumi, $TopOffset, $ClickPlayerAutoLocation, $AutoQuality, $Quality4K, $Quality8K, $Checkbox4K, $Checkbox8K, $WebfullUnlock, $AutoReload, $videoSettingSaveButton ] = await elmGetter.get([ selectors.IsVip, selectors.AutoLocate, selectors.AutoLocateVideo, selectors.AutoLocateBangumi, selectors.TopOffset, selectors.ClickPlayerAutoLocation, selectors.AutoQuality, selectors.Quality4K, selectors.Quality8K, selectors.Checkbox4K, selectors.Checkbox8K, selectors.WebfullUnlock, selectors.AutoReload, selectors.videoSettingSaveButton ])
         $videoSettingPopover.addEventListener('toggle', event => {
           if (event.newState === 'open') {
             // document.querySelector('*:not(#videoSettingPopover *)').style.pointerEvents = 'none'
