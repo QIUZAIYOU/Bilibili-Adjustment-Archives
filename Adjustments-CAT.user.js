0c4dd631609018e60187cd4d5fdf174479a7c075
QIAN <qian@asifadeaway.com>
2025-01-27 23:15:18 +0800

    refactor(insertVideoDescriptionToComment): 优化简介插入评论区显示效果

--- Adjustments-CAT.user.js
+++ Adjustments-CAT.user.js
@@ -3,7 +3,7 @@
 // @namespace         哔哩哔哩（bilibili.com）调整 For ScriptCat
 // @copyright         QIAN
 // @license           GPL-3.0 License
-// @version           0.1.38.2
+// @version           0.1.38.3
 // @description       一、首页新增推荐视频历史记录(仅记录前6个推荐位中的非广告内容)，以防误点刷新错过想看的视频。二、动态页调整：默认显示"投稿视频"内容，可自行设置URL以免未来URL发生变化。三、播放页调整：1.自动定位到播放器（进入播放页，可自动定位到播放器，可设置偏移量及是否在点击主播放器时定位）；2.可设置播放器默认模式；3.可设置是否自动选择最高画质；4.新增快速返回播放器漂浮按钮；5.新增点击评论区时间锚点可快速返回播放器；6.网页全屏模式解锁(网页全屏模式下可滚动查看评论，并在播放器控制栏新增快速跳转至评论区按钮)；7.将视频简介内容优化后插入评论区或直接替换原简介区内容(替换原简介中固定格式的静态内容为跳转链接)；8.视频播放过程中跳转指定时间节点至目标时间节点(可用来跳过片头片尾及中间广告等)；9.新增点击视频合集、下方推荐视频、结尾推荐视频卡片快速返回播放器；
 // @author            QIAN
 // @match             *://www.bilibili.com
@@ -1560,75 +1560,7 @@
         // const upAvatarDecorationLink = document.querySelector(selectors.upAvatarDecoration) ? document.querySelector(selectors.upAvatarDecoration).dataset.src.replace('@144w_144h_!web-avatar', '@240w_240h_!web-avatar-comment') : ''
         const shadowRootVideoDescriptionReplyTemplate = `
           <bili-adjustment-comment-thread-renderer>
-              <style>
-                  #bili-adjustment-body {
-                      position: relative;
-                      padding-left: 80px;
-                      padding-top: 22px;
-                  }
-
-                  #bili-adjustment-user-avatar {
-                      position: absolute;
-                      left: 20px;
-                      top: 22px;
-                      width: 48px;
-                      height: 48px;
-                      transform-origin: left top;
-                      transform: scale(1);
-                  }
-
-                  #bili-adjustment-avatar-picture {
-                      width: 48px;
-                      height: 48px;
-                      opacity: 1;
-                      border-radius: 50%;
-                  }
-
-                  #bili-adjustment-info {
-                      display: inline-flex;
-                      align-items: center;
-                  }
-
-                  #bili-adjustment-user-name {
-                      font-size: 13px;
-                      font-weight: 500;
-                  }
-
-                  #bili-adjustment-user-name a {
-                      color: #00a1d6;
-                      text-decoration: none;
-
-                  }
-
-                  #bili-adjustment-content {
-                      font-size: 15px;
-                      line-height: 24px;
-                  }
-
-                  #bili-adjustment-contents {
-                      margin-block-start: 0;
-                      margin-block-end: 0;
-                      margin-inline-start: 0;
-                      margin-inline-end: 0;
-                      display: inline;
-                      white-space: pre-line;
-                      word-break: break-word;
-                      -webkit-font-smoothing: antialiased;
-                  }
-
-                  #bili-adjustment-contents a {
-                      color: #00a1d6;
-                      text-decoration: none;
-                      background-color: transparent;
-                      cursor: pointer;
-                  }
-
-                  #bili-adjustment-spread-line {
-                      padding-bottom: 14px;
-                      margin-left: 80px;
-                      border-bottom: 1px solid var(--graph_bg_thick);
-                  }
-              </style>
+              <style>#bili-adjustment-body{position:relative;padding-left:80px;padding-top:22px}#bili-adjustment-user-avatar{position:absolute;left:20px;top:22px;width:48px;height:48px;transform-origin:left top;transform:scale(1)}#bili-adjustment-avatar-picture{width:48px;height:48px;opacity:1;border-radius:50%;border:2px solid #00a1d6}#bili-adjustment-info{display:inline-flex;align-items:center}#bili-adjustment-user-name{font-size:13px;font-weight:500}#bili-adjustment-user-name a{color:#00a1d6;text-decoration:none}#bili-adjustment-user-badge{background:#0491bf;border-radius:3px;color:#fff;padding:2px 3px;margin-left:5px;font-size:10px}#bili-adjustment-content{font-size:15px;line-height:24px}#bili-adjustment-contents{margin-block-start:0;margin-block-end:0;margin-inline-start:0;margin-inline-end:0;display:inline;white-space:pre-line;word-break:break-word;-webkit-font-smoothing:antialiased}#bili-adjustment-contents a{color:#00a1d6;text-decoration:none;background-color:transparent;cursor:pointer}#bili-adjustment-spread-line{padding-bottom:14px;margin-left:80px;border-bottom:1px solid var(--graph_bg_thick)}</style>
               <bili-adjustment-comment-renderer id="comment">
                   <div id="bili-adjustment-body" class="light">
                       <a id="bili-adjustment-user-avatar">
@@ -1641,8 +1573,9 @@
                               <bili-adjustment-comment-user-info>
                                   <div id="bili-adjustment-info">
                                       <div id="bili-adjustment-user-name">
-                                          <a href="#" onclick="event.preventDefault()">Bilibili调整丨视频简介已优化并插入评论区</a>
+                                          <a href="#" onclick="event.preventDefault()">视频简介君 (ﾉ≧∀≦)ﾉ</a>
                                       </div>
+                                      <div id="bili-adjustment-user-badge">Bilibili调整</div>
                                   </div>
                               </bili-adjustment-comment-user-info>
                           </div>
@@ -2732,7 +2665,7 @@
                   modules.clickPlayerAutoLocation,
                   modules.insertFloatSideNavToolsButton,
                   modules.clickVideoTimeAutoLocation,
-                  modules.unlockEpisodeSelector,
+                  // modules.unlockEpisodeSelector,
                   // modules.insertSetSkipTimeNodesButton,
                   // modules.insertSkipTimeNodesSwitchButton,
                   // modules.autoSkipTimeNodes,
